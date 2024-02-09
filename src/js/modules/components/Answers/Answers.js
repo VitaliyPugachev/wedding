852cb34649d9c3a5e21b4deca048f8df8120ab1d
VitaliyPugachev <pugachev.vt@gmail.com>
2024-02-09 20:43:55 +0300

    fixed open ansers

--- src/js/modules/components/Answers/Answers.js
+++ src/js/modules/components/Answers/Answers.js
@@ -7,26 +7,31 @@ export function Answers() {
     const root = document.querySelector('.answers');
     root.innerHTML += '<h2>Ответы на вопросы</h2>';
 
-    function toggleAnswer(selector) {
-        const element = document.querySelector(selector);
+    function toggleAnswer(answerSelector, arrowSelector) {
+        const element = document.querySelector(answerSelector);
 
         if (element.style.display === 'none' || !element.style.display) {
             element.style.display = 'block';
         } else {
             element.style.display = 'none';
         }
+
+        const arrow = document.querySelector(arrowSelector);
+        arrow.classList.toggle('rotate');
     }
 
     questions.forEach((question, index) => {
         root.insertAdjacentHTML('beforeend', `    
             <button class="answers__question${index} answers__btn">
                 <h3>${question[0]}</h3>
-                ${arrowIcon}
+                <div class="answers__arrow${index}">
+                    ${arrowIcon}
+                </div>
             </button>
             <div class="answers__answerItem${index} answerWrapper"><p>${question[1]}</p></div>`);
 
         const btn = document.querySelector(`.answers__question${index}`);
-        btn.addEventListener('click', () => toggleAnswer(`.answers__answerItem${index}`));
+        btn.addEventListener('click', () => toggleAnswer(`.answers__answerItem${index}`, `.answers__arrow${index}`));
     });
 
     return root;
